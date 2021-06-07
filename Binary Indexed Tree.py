9e564907fa83e64758aca730e57fae6f3d81ec76
AlexLZM <58406527+AlexLZM@users.noreply.github.com>
2021-06-07 13:24:10 +0800

    Update Binary Indexed Tree.py

--- Binary Indexed Tree.py
+++ Binary Indexed Tree.py
@@ -10,25 +10,25 @@ class BIT:
         for i in range(n):
             self.update(i, arr[i])
 
-        def update(self, i, diff):
-            '''
-            update the value at original array's index i by a difference
-            '''
-            i += 1 # convert to 1-index
-            while i <= self.n:
+    def update(self, i, diff):
+        '''
+        update the value at original array's index i by a difference
+        '''
+        i += 1 # convert to 1-index
+        while i <= self.n:
 
-                self.tree[i] += diff
-                i += (i & (-i))
+            self.tree[i] += diff
+            i += (i & (-i))
 
-        def query(self, i):
-            '''
-            Get the sum of range [0,i]
-            (including i)
-            '''
-            i += 1 # convert to 1-index
-            res = 0
-            while i>0:
-                res += self.tree[i]
-                i -= (i & (-i))
-            return res
+    def query(self, i):
+        '''
+        Get the sum of range [0,i]
+        (including i)
+        '''
+        i += 1 # convert to 1-index
+        res = 0
+        while i>0:
+            res += self.tree[i]
+            i -= (i & (-i))
+        return res
 
